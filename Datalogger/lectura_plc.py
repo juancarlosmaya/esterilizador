d11f2573f41a8fb4ef7a2cd4dc44aa84602ab193
juancarlosmaya <juancarlosmayag@gmail.com>
2023-12-05 14:44:57 -0500

    creación de multiples archivos

--- Datalogger/lectura_plc.py
+++ Datalogger/lectura_plc.py
@@ -1,39 +1,70 @@
 import serial
 import time
-
 import fpdf
 
-# Create a new PDF document
-pdf = fpdf.FPDF()
-# Set the font
-pdf.set_font('Arial', 'B', 10)
-pdf.set_line_width(0.01)
-# Add a page
-pdf.add_page()
 
-
-
+NUMERO_DE_ARCHCIVO=0
 # Open the serial port
 print("abriendo pueto")
 try:
     ser = serial.Serial(port='COM7', bytesize=8, parity='E', baudrate = 19600, stopbits=1,timeout=0.5)
+    
+    while True:
+        line2= ""
+        line =""
+        while not ("FECHA" in  line):
+            print("...")
+            line = ser.read_until()                             # lectura de cadena recibida en binario                                
+            line = line.decode('Latin-1').replace("\r", "\n")   # conversion a cadena de texto, remplazando retorno de carro por nueva linea
+        
+        print("TIENE LA PALABRA FECHA")
+        
+        # Create a new PDF document
+        pdf = fpdf.FPDF()
+        # Set the font
+        pdf.set_font('Arial', 'B', 10)
+        pdf.set_line_width(0.01)
+        # Add a page
+        pdf.add_page()
+
+        while not ("DESCARGA" in  line):
+            print("LA CANTIDAD DE BYTES ACUMULADOS SON: ")
+            print(ser.in_waiting)
+            line2 = line2+line
+            line = ser.read_until()                             # lectura de cadena recibida en binario 
+            line = line.decode('Latin-1').replace("\r", "\n")   # conversion a cadena de texto, remplazando retorno de carro por nueva linea
+            time.sleep(0.5)
+
+        print("TIENE LA PALABRA DESCARGA")
+
+        line2 = line2+line
+        
+        pdf.write(5,line2)
+        pdf.output(str(NUMERO_DE_ARCHCIVO)+'.pdf')
+        NUMERO_DE_ARCHCIVO=NUMERO_DE_ARCHCIVO+1
+        time.sleep(0.5)
 except:
     print("error")
 
 print(ser)
 
+
+
+
+
+
+
 # Create an empty dictionary to store the strings
-data = {}
-line2= ""
+#data = {}
+
 
 # Continuously read strings from the serial port and add them to the dictionary
-for x in range(0,5):
+#for x in range(0,5):
     # Read a line of data from the serial port
     
-    line = ser.read_until()                         # lectura de cadena recibida en binario                                
-    line = line.decode('Latin-1').replace("\r", "\n") # conversion a cadena de texto, remplazando retorno de carro por nueva linea
     
-    line2 = line2+line
+    
+    
     #pdf.multi_cell(0,10,line)
     #if line:
     #    pdf.write(10,line)
@@ -62,10 +93,8 @@ for x in range(0,5):
     #    pdf.output('hello_world.pdf')
 
 
-    time.sleep(5)
-    print("LA CANTIDAD DE BYTES ACUMULADOS SON: ")
-    print(ser.in_waiting)
-pdf.write(5,line2)
-pdf.output('hello_world.pdf')
+#    time.sleep(5)
+    
+
 
         
